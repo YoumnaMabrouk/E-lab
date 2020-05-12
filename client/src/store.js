85493821f5ef6fac2b2b0176d7517303ecb295f8
nihalmrashed <nehalmrashed@aol.com>
2020-05-12 07:51:58 +0200

    deploying

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,3 @@
-
 import {createStore,applyMiddleware,compose} from 'redux';
 //import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
@@ -8,10 +7,14 @@ import rootReducer from './reducers';
 const initialState ={};
 const middleware=[thunk];
 const store =createStore(rootReducer,initialState,compose(applyMiddleware(...middleware),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+window._REDUX_DEVTOOLS_EXTENSION_
+? window._REDUX_DEVTOOLS_EXTENSION_()
+: f => f));
 
 const store1=createStore(rootReducer,initialState,compose(applyMiddleware(...middleware),
-window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+window._REDUX_DEVTOOLS_EXTENSION_
+? window._REDUX_DEVTOOLS_EXTENSION_()
+: f => f));
 
 
 export default store;
